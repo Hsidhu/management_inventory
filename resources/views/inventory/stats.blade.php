ed39cddc7178819e050afeda3eab21ec187ff373
Harry Sidhu <firstwish13@gmail.com>
2023-01-25 00:11:07 -0500

    Update stats.blade.php

--- resources/views/inventory/stats.blade.php
+++ resources/views/inventory/stats.blade.php
@@ -5,7 +5,7 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">
-                    <h4 class="card-title">Statistics by Quantity (TOP 15)</h4>
+                    <h4 class="card-title">Average Product Price by Quantity (TOP 15)</h4>
                 </div>
                 <div class="card-body">
                     <table class="table">
@@ -14,21 +14,17 @@
                             <th>Category</th>
                             <th>Name</th>
                             <th>Stock</th>
-                            <th>Annual Sales</th>
                             <th>Average Price</th>
-                            <th>Annual Income</th>
                             <th></th>
                         </thead>
                         <tbody>
-                            @foreach($soldproductsbystock as $soldproduct)
+                            @foreach($avgProductPrice as $soldproduct)
                                 <tr>
                                     <td><a href="{{ route('products.show', $soldproduct->product) }}">{{ $soldproduct->product_id }}</a></td>
                                     <td><a href="{{ route('categories.show', $soldproduct->product->category) }}">{{ $soldproduct->product->category->name }}</a></td>
                                     <td>{{ $soldproduct->product->name }}</td>
-                                    <td>{{ $soldproduct->product->stock }}</td>
                                     <td>{{ $soldproduct->total_qty }}</td>
                                     <td>{{ format_money(round($soldproduct->avg_price, 2)) }}</td>
-                                    <td>{{ format_money($soldproduct->incomes) }}</td>
                                     <td class="td-actions text-right">
                                         <a href="{{ route('products.show', $soldproduct->product) }}" class="btn btn-link" data-toggle="tooltip" data-placement="bottom" title="More Details">
                                             <i class="tim-icons icon-zoom-split"></i>
@@ -46,7 +42,7 @@
         <div class="col-md-6">
             <div class="card card-tasks">
                 <div class="card-header">
-                    <h4 class="card-title">Statistics by Income (TOP 15)</h4>
+                    <h4 class="card-title">Product used by Qty (TOP 15)</h4>
                 </div>
                 <div class="card-body">
                     <div class="table-full-width table-responsive">
@@ -55,11 +51,10 @@
                                 <th>ID</th>
                                 <th>Category</th>
                                 <th>Name</th>
-                                <th>Sold</th>
-                                <th>Income</th>
+                                <th>Qty Used</th>
                             </thead>
                             <tbody>
-                                @foreach ($soldproductsbyincomes as $soldproduct)
+                                @foreach ($topProductsbyUsedStock as $soldproduct)
                                     <tr>
                                         <td>{{ $soldproduct->product_id }}</td>
                                         <td><a href="{{ route('categories.show', $soldproduct->product->category) }}">{{ $soldproduct->product->category->name }}</a></td>
@@ -86,11 +81,11 @@
                                 <th>ID</th>
                                 <th>Category</th>
                                 <th>Name</th>
-                                <th>Sold</th>
+                                <th>Stocked</th>
                                 <th>Average Price</th>
                             </thead>
                             <tbody>
-                                @foreach ($soldproductsbyavgprice as $soldproduct)
+                                @foreach ($topProductsReceivedbyStock as $soldproduct)
                                     <tr>
                                         <td>{{ $soldproduct->product_id }}</td>
                                         <td><a href="{{ route('categories.show', $soldproduct->product->category) }}">{{ $soldproduct->product->category->name }}</a></td>
