f7b5d3bb16a517b0ea4903abb80526c1ef9a494f
Harry Sidhu <firstwish13@gmail.com>
2023-01-25 00:09:01 -0500

    Update InventoryController.php

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers;
 use App\Product;
 use Carbon\Carbon;
 use App\SoldProduct;
+use App\ReceivedProduct;
 use App\ProductCategory;
-use Illuminate\Http\Request;
 
 class InventoryController extends Controller
 {
@@ -15,9 +15,42 @@ class InventoryController extends Controller
         return view('inventory.stats', [
             'categories' => ProductCategory::all(),
             'products' => Product::all(),
-            'soldproductsbystock' => SoldProduct::selectRaw('product_id, max(created_at), sum(qty) as total_qty, sum(total_amount) as incomes, avg(price) as avg_price')->whereYear('created_at', Carbon::now()->year)->groupBy('product_id')->orderBy('total_qty', 'desc')->limit(15)->get(),
-            'soldproductsbyincomes' => SoldProduct::selectRaw('product_id, max(created_at), sum(qty) as total_qty, sum(total_amount) as incomes, avg(price) as avg_price')->whereYear('created_at', Carbon::now()->year)->groupBy('product_id')->orderBy('incomes', 'desc')->limit(15)->get(),
-            'soldproductsbyavgprice' => SoldProduct::selectRaw('product_id, max(created_at), sum(qty) as total_qty, sum(total_amount) as incomes, avg(price) as avg_price')->whereYear('created_at', Carbon::now()->year)->groupBy('product_id')->orderBy('avg_price', 'desc')->limit(15)->get()
+            'avgProductPrice' => $this->getProductAvgPrice(),
+            'topProductsbyUsedStock' => $this->topProductsbyUsedStock(),
+            'topProductsReceivedbyStock' => $this->topProductsReceivedbyStock()
         ]);
+    }
+
+    protected function topProductsbyUsedStock()
+    {
+        return SoldProduct::selectRaw('product_id, max(created_at), sum(qty) as total_qty')
+        ->whereHas('sale', function ($query) {
+            $query->where('finalized_at', '<>' , NULL);
+        })
+        ->whereYear('created_at', Carbon::now()->year)
+        ->groupBy('product_id')
+        ->orderBy('total_qty', 'desc')->limit(15)->get();
+    }
+
+    protected function topProductsReceivedbyStock()
+    {
+        return ReceivedProduct::selectRaw('product_id, max(created_at), avg(price) as avg_price, sum(stock) as total_qty')
+        ->whereHas('receipt', function ($query) {
+            $query->where('finalized_at', '<>' , NULL);
+        })
+        ->whereYear('created_at', Carbon::now()->year)
+        ->groupBy('product_id')
+        ->orderBy('total_qty', 'desc')->limit(15)->get();
+    }
+
+    protected function getProductAvgPrice()
+    {
+        return ReceivedProduct::selectRaw('product_id, max(created_at), avg(price) as avg_price, sum(stock) as total_qty' )
+        ->whereHas('receipt', function ($query) {
+            $query->where('finalized_at', '<>' , NULL);
+        })
+        ->whereYear('created_at', Carbon::now()->year)
+        ->groupBy('product_id')
+        ->orderBy('avg_price', 'desc')->limit(15)->get();
     }
 }
