32811e23b4931d22adec35221d545fc048ad6b8c
Harry Sidhu <firstwish13@gmail.com>
2023-01-25 21:01:59 -0500

    Update show.blade.php

--- resources/views/inventory/products/show.blade.php
+++ resources/views/inventory/products/show.blade.php
@@ -5,7 +5,14 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">
-                    <h4 class="card-title">Product Information</h4>
+                    <div class="row">
+                        <div class="col-8">
+                            <h4 class="card-title">Product Information</h4>
+                        </div>
+                        <div class="col-4 text-right">
+                            <a href="{{ route('products.index') }}" class="btn btn-sm btn-primary">Product List</a>
+                        </div>
+                    </div>
                 </div>
                 <div class="card-body">
                     <table class="table">
@@ -15,22 +22,18 @@
                             <th>Name</th>
                             <th>Stock</th>
                             <th>Defective Stock</th>
-                            <th>Base price</th>
                             <th>Average Price</th>
-                            <th>Total sales</th>
-                            <th>Income Produced</th>
+                            <th>Total Checkout</th>
                         </thead>
                         <tbody>
                             <tr>
                                 <td>{{ $product->id }}</td>
                                 <td><a href="{{ route('categories.show', $product->category) }}">{{ $product->category->name }}</a></td>
                                 <td>{{ $product->name }}</td>
-                                <td>{{ $product->stock }}</td>
-                                <td>{{ $product->stock_defective }}</td>
-                                <td>{{ format_money($product->price) }}</td>
-                                <td>{{ format_money($product->solds->avg('price')) }}</td>
+                                <td>{{ $product->balance->stock }}</td>
+                                <td>{{ $product->balance->stock_defective }}</td>
+                                <td>{{ format_money($product->received->avg('price')) }}</td>
                                 <td>{{ $product->solds->sum('qty') }}</td>
-                                <td>{{ format_money($product->solds->sum('total_amount')) }}</td>
                             </tr>
                         </tbody>
                     </table>
@@ -43,26 +46,24 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-body">
-                    <h4 class="card-title">Latest Sales</h4>
+                    <h4 class="card-title">Latest Checkout</h4>
                 </div>
                 <div class="card-body">
                     <table class="table">
                         <thead>
-                            <th>Date</th>
                             <th>Sale ID</th>
                             <th>Quantity</th>
-                            <th>Price Unit</th>
-                            <th>Total Amount</th>
+                            <th>User</th>
+                            <th>Date</th>
                             <th></th>
                         </thead>
                         <tbody>
                             @foreach ($solds as $sold)
                                 <tr>
-                                    <td>{{ date('d-m-y', strtotime($sold->created_at)) }}</td>
                                     <td><a href="{{ route('sales.show', $sold->sale_id) }}">{{ $sold->sale_id }}</a></td>
                                     <td>{{ $sold->qty }}</td>
-                                    <td>{{ format_money($sold->price) }}</td>
-                                    <td>{{ format_money($sold->total_amount) }}</td>
+                                    <td>{{ $sold->sale->user->name }}</td>
+                                    <td>{{ date('d-m-y', strtotime($sold->created_at)) }}</td>
                                     <td class="td-actions text-right">
                                         <a href="{{ route('sales.show', $sold->sale_id) }}" class="btn btn-link" data-toggle="tooltip" data-placement="bottom" title="View Sale">
                                             <i class="tim-icons icon-zoom-split"></i>
@@ -81,27 +82,27 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-body">
-                    <h4 class="card-title">Latest Receipts</h4>
+                    <h4 class="card-title">Latest Received</h4>
                 </div>
                 <div class="card-body">
                     <table class="table">
                         <thead>
-                            <th>Date</th>
                             <th>Receipt ID</th>
                             <th>Title</th>
                             <th>Stock</th>
                             <th>Defective Stock</th>
+                            <th>Date</th>
                             <th>Total Stock</th>
                             <th></th>
                         </thead>
                         <tbody>
-                            @foreach ($receiveds as $received)
+                            @foreach ($productReceived as $received)
                                 <tr>
-                                    <td>{{ date('d-m-y', strtotime($received->created_at)) }}</td>
                                     <td><a href="{{ route('receipts.show', $received->receipt) }}">{{ $received->receipt_id }}</a></td>
                                     <td style="max-width:150px;">{{ $received->receipt->title }}</td>
                                     <td>{{ $received->stock }}</td>
                                     <td>{{ $received->stock_defective }}</td>
+                                    <td>{{ date('d-m-y', strtotime($received->created_at)) }}</td>
                                     <td>{{ $received->stock + $received->stock_defective }}</td>
                                     <td class="td-actions text-right">
                                         <a href="{{ route('receipts.show', $received->receipt) }}" class="btn btn-link" data-toggle="tooltip" data-placement="bottom" title="Ver Receipt">
